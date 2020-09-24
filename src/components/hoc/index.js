1611a85d40d6d5fa5ab5dee5f393e92c358b991b
Kalim <kalim.ahmethin2003@gamil.com>
2020-09-24 20:33:25 +0300

    initial react-redux project configurations

---
+++ src/components/hoc/index.js
@@ -0,0 +1,5 @@
+import withBookstoreService from './with-bookstore-service-context';
+
+export {
+    withBookstoreService
+};
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,13 @@
+export default class BookstoreService {
+    getBooks() {
+        return [
+            { id: 1,
+              title: 'Witcher I',
+              author: 'Andrzej Sapkowski' },
+            { id: 2,
+              title: 'Witcher II',
+              author: 'Andrzej Sapkowski' },
+        ];
+    }
+    
+}
