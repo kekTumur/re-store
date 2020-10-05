caaea6ee06c0270ad8e39b044c77ab2690a2f2c5
Kalim <kalim.ahmethin2003@gamil.com>
2020-10-05 21:17:43 +0300

    last commit

--- src/store.js
+++ src/store.js
@@ -1,6 +1,29 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (dispatch) => (action) => {
+    console.log(action.type);
+    return dispatch(action);
+}
+
+const stringMiddleware = () => (dispatch) => (action) => {
+    if (typeof action === 'string') {
+        return dispatch({
+            type: action
+        });
+    }
+    return dispatch(action);
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware ,stringMiddleware, logMiddleware));
+
+const delayedActionCreator = (timeout) => (dispatch) => {
+    setTimeout(() => dispatch({
+        type: 'DELAYED_ACTION'
+    }), timeout);
+}
+
+store.dispatch(delayedActionCreator(3000));
 
 export default store;
